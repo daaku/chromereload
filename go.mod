9a28c4d1c7d1192cf53260d1266a8cab6f9fdbc2
Naitik Shah <n@daaku.org>
2020-04-13 20:59:54 +0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/daaku/chromereload
+
+go 1.14
+
+require (
+	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e
+	github.com/pkg/errors v0.9.1
+)
---
+++ main.go
@@ -0,0 +1,91 @@
+// Command chromereload reloads the current tab.
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/pkg/errors"
+	"golang.org/x/net/websocket"
+)
+
+var reloadCmd = []byte(`
+{
+	"id": 1,
+	"method": "Page.reload",
+	"params": {
+		"ignoreCache": true
+	}
+}
+`)
+
+type app struct {
+	host string
+	port int
+}
+
+func (a *app) debuggerURL() string {
+	return fmt.Sprintf("http://%s:%d/json", a.host, a.port)
+}
+
+func (a *app) wsURL() (string, error) {
+	res, err := http.Get(a.debuggerURL())
+	if err != nil {
+		return "", errors.WithStack(err)
+	}
+	defer res.Body.Close()
+
+	var tabs []struct {
+		WebSocketDebuggerURL string `json:"WebSocketDebuggerUrl"`
+	}
+	if err := json.NewDecoder(res.Body).Decode(&tabs); err != nil {
+		return "", errors.WithStack(err)
+	}
+	return tabs[0].WebSocketDebuggerURL, nil
+}
+
+func (a *app) reload() error {
+	wsURL, err := a.wsURL()
+	if err != nil {
+		return err
+	}
+
+	origin := fmt.Sprintf("http://%s/", a.host)
+	ws, err := websocket.Dial(wsURL, "", origin)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	defer ws.Close()
+
+	if _, err := ws.Write(reloadCmd); err != nil {
+		return errors.WithStack(err)
+	}
+
+	msg := make([]byte, 1024)
+	n, err := ws.Read(msg)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	log.Printf("response: %s\n", msg[:n])
+	return nil
+}
+
+func run() error {
+	var a app
+	fs := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	fs.StringVar(&a.host, "host", "localhost", "remote debugger host")
+	fs.IntVar(&a.port, "port", 9222, "remote debugger port")
+	fs.Parse(os.Args[1:])
+	return a.reload()
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "%+v\n", err)
+		os.Exit(1)
+	}
+}
